090abcd05124f8d19e08c26ae2d24c6e8345eb30
xianren <xianren711@qq.com>
2025-01-11 21:51:41 +0800

    auto construct blog

---
+++ assets/zig_index.md.1Tm3x-gq.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as a}from"./chunks/framework.C62L6tH-.js";const l=JSON.parse('{"title":"zig语言学习","description":"","frontmatter":{"layout":"default","title":"zig语言学习"},"headers":[],"relativePath":"zig/index.md","filePath":"zig/index.md"}'),i={name:"zig/index.md"};function n(o,r,s,c,d,p){return a(),t("div")}const f=e(i,[["render",n]]);export{l as __pageData,f as default};
